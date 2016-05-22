b4aa60400264601b29029034fde3e091e3d7db11
Malysheva Sasha <malyshevasasha777@gmail.com>
2016-05-22 15:36:46 +0300

    Now FeatureMatching doesn't show pictures

--- FeatureMatchingPoint/main.cpp
+++ FeatureMatchingPoint/main.cpp
@@ -42,15 +42,8 @@ int main( int argc, char** argv )
     detector->detect( img_1, keypoints_1 );
     detector->detect( img_2, keypoints_2 );
 
-    //-- Draw keypoints
-    Mat img_keypoints_1; Mat img_keypoints_2;
 
-    drawKeypoints( img_1, keypoints_1, img_keypoints_1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
-    drawKeypoints( img_2, keypoints_2, img_keypoints_2, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
-
-    //-- Show detected (drawn) keypoints
-    imshow("Keypoints 1", img_keypoints_1 );
-    imshow("Keypoints 2", img_keypoints_2 );
+    //-- Write detected  keypoints
     for(size_t i = 0; i <  keypoints_1.size(); i++)
     {
         fout
