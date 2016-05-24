a0afded6075a7de8f5c2bf22c6d997ce9f9e7205
Anastasia Gaydashenko <a.v.gaydashenko@gmail.com>
2016-05-24 06:58:55 +0300

    video stream works

--- SaveImage/src/sample/FXController.java
+++ SaveImage/src/sample/FXController.java
@@ -1,24 +1,20 @@
 package sample;
 
-import com.leapmotion.leap.*;
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Frame;
 import com.leapmotion.leap.Image;
+import com.leapmotion.leap.ImageList;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
-import javafx.scene.image.*;
+import javafx.scene.image.ImageView;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.image.BufferedImage;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+import java.io.File;
+import java.io.IOException;
 
 public class FXController {
     public Button buttonStartReceivingFrames;
@@ -28,114 +24,10 @@ public class FXController {
     public ImageView imageFromDelta;
     public ImageView imageFromHeatMap;
     public MenuBar menuBar;
-    protected final BlockingQueue<Frame> queue = new ArrayBlockingQueue<>(1);
-    private Timer timer;
     private String algorithm = "bm";
 
     private File file1 = new File("image1.png");
     private File file2 = new File("image2.png");
-
-    class Task extends TimerTask {
-        public void run() {
-            Frame frame = null;
-            try {
-                frame = queue.take();
-            } catch (InterruptedException ignored) {
-            }
-            System.out.println("Frame has taken");
-            if (frame != null && frame.isValid()) {
-                ImageList images = frame.images();
-
-                // width: 640; height: 240
-                final Image image1 = images.get(0);
-                final Image image2 = images.get(1);
-
-                if (image1.isValid() && image2.isValid()) {
-
-                    int width = image1.width();
-                    int height = image1.height();
-
-                    javafx.scene.image.Image fxImage1 = JavaFXImageConversion.getJavaFXImage(image1.data(),
-                            width, height);
-                    javafx.scene.image.Image fxImage2 = JavaFXImageConversion.getJavaFXImage(image2.data(),
-                            width, height);
-
-                    imageFromFirstCamera.setImage(fxImage1);
-                    imageFromSecondCamera.setImage(fxImage2);
-
-                    int size = height * width;
-                    byte[] data1 = image1.data();
-                    byte[] data2 = image2.data();
-                    byte[] delta = new byte[size];
-                    for (int i = 0; i < size; i++) {
-                        delta[i] = (byte) (data1[i] - data2[i]);
-                    }
-
-                    imageFromDelta.setImage(JavaFXImageConversion.getJavaFXImage(delta, width, height));
-
-                    BufferedImage bImage1 = SwingFXUtils.fromFXImage(fxImage1, null);
-                    BufferedImage bImage2 = SwingFXUtils.fromFXImage(fxImage2, null);
-
-                    try {
-                        ImageIO.write(bImage1, "png", file1);
-                        ImageIO.write(bImage2, "png", file2);
-                    } catch (IOException e) {
-                        System.out.println("Failed to save images.");
-                    }
-
-                    Thread t = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                String[] cmdArray = new String[5];
-                                cmdArray[0] = "bin\\FeatureMatching.exe";
-                                cmdArray[1] = "image1.png";
-                                cmdArray[2] = "image2.png";
-                                cmdArray[3] = "src/resources/dst_path.png";
-                                cmdArray[4] = "--no-display";
-                                Process process = Runtime.getRuntime().exec(cmdArray,null);
-                                int res = process.waitFor();
-                                System.out.println("DONE");
-                            } catch (Exception ignored) {
-                                System.out.println("Failed to run shell.");
-                            }
-                        }
-                    });
-                    t.start();
-                    try {
-                        t.join();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("START");
-                    javafx.scene.image.Image image = new javafx.scene.image.Image(FXController.class.getResourceAsStream("/resources/dst_path.png"));
-                    imageFromHeatMap.setImage(image);
-                    controller.removeListener(listener);
-                    System.out.println("Created");
-                    timer.cancel();
-                    thread.interrupt();
-                }
-            }
-        }
-    }
-
-    private final Thread thread = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            while (!thread.isInterrupted()) {
-                Frame frame = controller.frame();
-                System.out.println("MyFrame id: " + frame.id());
-                try {
-                    queue.put(frame);
-                } catch (InterruptedException e) {
-                    /*e.printStackTrace();
-                    Thread.currentThread().interrupt();*/
-                }
-            }
-        }
-    });
-
-
 
     private void setAlgo(String algorithm) {
         this.algorithm = algorithm;
@@ -152,6 +44,87 @@ public class FXController {
     private final SampleListener listener = new SampleListener();
     private Controller controller = null;
 
+    private final Thread thread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            while (!thread.isInterrupted()) {
+                Frame frame = null;
+                frame = controller.frame();
+                System.out.println("Frame has taken");
+                if (frame != null && frame.isValid()) {
+                    ImageList images = frame.images();
+
+                    // width: 640; height: 240
+                    final Image image1 = images.get(0);
+                    final Image image2 = images.get(1);
+
+                    if (image1.isValid() && image2.isValid()) {
+
+                        int width = image1.width();
+                        int height = image1.height();
+
+                        javafx.scene.image.Image fxImage1 = JavaFXImageConversion.getJavaFXImage(image1.data(),
+                                width, height);
+                        javafx.scene.image.Image fxImage2 = JavaFXImageConversion.getJavaFXImage(image2.data(),
+                                width, height);
+
+                        imageFromFirstCamera.setImage(fxImage1);
+                        imageFromSecondCamera.setImage(fxImage2);
+
+                        int size = height * width;
+                        byte[] data1 = image1.data();
+                        byte[] data2 = image2.data();
+                        byte[] delta = new byte[size];
+                        for (int i = 0; i < size; i++) {
+                            delta[i] = (byte) (data1[i] - data2[i]);
+                        }
+
+                        imageFromDelta.setImage(JavaFXImageConversion.getJavaFXImage(delta, width, height));
+
+                        BufferedImage bImage1 = SwingFXUtils.fromFXImage(fxImage1, null);
+                        BufferedImage bImage2 = SwingFXUtils.fromFXImage(fxImage2, null);
+
+                        try {
+                            ImageIO.write(bImage1, "png", file1);
+                            ImageIO.write(bImage2, "png", file2);
+                        } catch (IOException e) {
+                            System.out.println("Failed to save images.");
+                        }
+
+                        Thread t = new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    String[] cmdArray = new String[5];
+                                    cmdArray[0] = "bin\\FeatureMatching.exe";
+                                    cmdArray[1] = "image1.png";
+                                    cmdArray[2] = "image2.png";
+                                    cmdArray[3] = "dst_path.png";
+                                    cmdArray[4] = "--no-display";
+                                    Process process = Runtime.getRuntime().exec(cmdArray, null);
+                                    int res = process.waitFor();
+                                    System.out.println("DONE");
+                                } catch (Exception ignored) {
+                                    System.out.println("Failed to run shell.");
+                                }
+                            }
+                        });
+                        t.start();
+                        try {
+                            t.join();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("START");
+                        javafx.scene.image.Image image = new javafx.scene.image.Image("file:dst_path.png");
+                        imageFromHeatMap.setImage(image);
+                        System.out.println("Created");
+                    }
+                }
+            }
+        }
+    });
+
     public void onClickStart(ActionEvent actionEvent) {
         controller = new Controller();
         while (!controller.isConnected()) {
@@ -165,16 +138,13 @@ public class FXController {
 
         controller.setPolicy(Controller.PolicyFlag.POLICY_IMAGES);
         controller.addListener(listener);
-
-        timer = new Timer();
-        timer.schedule(new Task(), 0, 2000);
         System.out.println("test");
+
         thread.start();
     }
 
     public void onClickStop(ActionEvent actionEvent) {
         controller.removeListener(listener);
-        timer.cancel();
         thread.interrupt();
     }
 }
